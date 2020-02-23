172ccb1a39bb1755c0b10fb77f3db2ff930766ba
Riley <rileyk42@icloud.com>
2020-02-23 02:05:20 +0000

    Added base code

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import pygame
 
+pygame.init();
+
+pygame.font.init();
+
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+display_info = pygame.display.Info()
+size = width, height = display_info.current_w, display_info.current_h
+
+while True:
+  screen.fill(pygame.Color(255, 255, 255))
+
+  pygame.display.update()
